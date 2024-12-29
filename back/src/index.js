a1e4dfea1d8d6bd67ac4326ce15b781877210455
VictorMGC2912 <victor89_13@hotmail.com>
2024-12-29 11:00:46 +0100

    configure conection with mongo success

---
+++ back/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+
+const app = express();
+const port = 9000;
+
+//CONEXION CON MONGO
+const url_mongo = 'mongodb+srv://victor8913:Q76N1C5v3BDGtwKK@cluster0.5fe8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(url_mongo);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+    console.log(`Error al conectar con mongo ${error}`)
+});
+db.on("connected", () => {
+    console.log(`Succecss connect`)
+});
+db.on("disconected", () => {
+    console.log(`Mongo is disconected`)
+});
+
+app.use(express.json());
+app.use(cors());
+
+//RUTAS
+
+
+//ESCUCHA DEL PUERTO
+app.listen(port, () => {
+    console.log(`APP listening on port ${port}`);
+});
