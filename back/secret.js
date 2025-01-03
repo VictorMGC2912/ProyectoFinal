de2cd96cf808d2698b6650c335941d7cb99a99aa
VictorMGC2912 <victor89_13@hotmail.com>
2025-01-03 06:05:41 +0100

    Eliminar secret.js del repositorio y añadir al .gitignore

--- back/secret.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto");
-
-const secret = "token mega secreto de la app";
-
-const hash = crypto.createHmac("sha256", secret).update("Soy otro campo secreto").digest("hex");
-
-console.log(hash);
