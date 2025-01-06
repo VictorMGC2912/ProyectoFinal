6282cd7892c3c1b43bf98d6132895eca02265368
VictorMGC2912 <victor89_13@hotmail.com>
2025-01-06 10:46:24 +0100

    created carFetch & CarsComponents

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,9 +1,13 @@
 import { getAllCars } from "@/api/carsFetch";
+import CreatedCarComponent from "@/components/CarsComponents/CreatedCarComponent";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [cars, setCars] = useState([]);
+  const [carId, setCarId] = useState(null);
+  const [carHasChanged, setCarHasChanged] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
 
   const getAllCarsAux = async () => {
     const carsAux = await getAllCars();
@@ -15,8 +19,46 @@ export default function Home() {
     getAllCarsAux();
   }, []);
 
+  useEffect(() => {
+    getAllCarsAux();
+    setCarHasChanged(false);
+    closeCarDetails();
+  }, [carHasChanged]);
+
+  const handlerOnClick = (id) => {
+    setCarId(id);
+  };
+
+  const closeCarDetails = () => {
+    setCarId(null);
+  };
+
+  const handlerCreateCar = () => {
+    setIsCreating(true);
+  };
+
+  const closeCarCreation = () => {
+    setIsCreating(false);
+  };
+
   return (
     <>
+    <div className="home-container">
+      <h1 className="home-title">APP COCHES</h1>
+      <div className="home-actions">
+        {!isCreating ? (
+          <button className="create-button" onClick={handlerCreateCar}>
+            Crear Coche
+          </button>
+        ): (
+          <CreatedCarComponent
+            setCarHasChanged={setCarHasChanged}
+            carHasChanged={carHasChanged}
+            closeCarCreation={closeCarCreation}
+          />
+        )}
+        <hr />
+      </div>
       <div className="cars-list">
         {cars &&
           cars.map((car, index) => {
@@ -29,6 +71,7 @@ export default function Home() {
             );
           })}
       </div>
+    </div>
     </>
   );
 }
